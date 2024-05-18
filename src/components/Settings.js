04ebd4d0566eb0d3f87812c9a85c579c1d6aab25
samwanyua <samexample8@gmail.com>
2024-05-18 23:26:46 +0300

    Feat: Style Settings and My routes

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -88,7 +88,7 @@ const Settings = ({ setAuthorized }) => {
   return (
     <div className="flex flex-col lg:flex-row justify-center items-center lg:items-start min-h-screen py-16 mb-36 gap-8 lg:gap-36">
       <form className="flex flex-col items-center justify-start gap-8 p-6 md:p-10 lg:p-16 xl:p-20 max-w-[90%] sm:max-w-[80%] md:max-w-[70%] lg:max-w-[50%] xl:max-w-[40%] shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] bg-white rounded-xl">
-        <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-darkslateblue-300 mb-4">
+        <h1 className="text-2xl md:text-3xl lg:text-3xl text-center font-bold text-darkslateblue-300 mb-4">
           Profile settings
         </h1>
         <div className="w-[120px] h-[120px] rounded-full flex items-center justify-center pt-[84px] pb-2 bg-[url('/public/39@2x.png')] bg-cover bg-no-repeat bg-[top]">
@@ -107,7 +107,7 @@ const Settings = ({ setAuthorized }) => {
         <b className="text-xl text-darkslateblue-300">Notification Preferences</b>
         <div className="flex flex-col gap-3">
           <div className="flex items-center justify-between">
-            <div className="font-medium text-base">Route Updates</div>
+            <div className="font-medium text-base mr-2">Route Updates</div>
             {notifications.routeUpdates ? (
               <BiToggleRight
                 className="text-gray-900 cursor-pointer text-3xl ml-2"
@@ -121,7 +121,7 @@ const Settings = ({ setAuthorized }) => {
             )}
           </div>
           <div className="flex items-center justify-between">
-            <div className="font-medium text-base">Product Insights</div>
+            <div className="font-medium text-base mr-2">Product Insights</div>
             {notifications.productInsights ? (
               <BiToggleRight
                 className="text-gray-900 cursor-pointer text-3xl ml-2 "
@@ -135,7 +135,7 @@ const Settings = ({ setAuthorized }) => {
             )}
           </div>
           <div className="flex items-center justify-between">
-            <div className="font-medium text-base">Sales orders</div>
+            <div className="font-medium text-base mr-2">Sales orders</div>
             {notifications.salesOrders ? (
               <BiToggleRight
                 className="text-gray-900 cursor-pointer text-3xl"
@@ -149,7 +149,7 @@ const Settings = ({ setAuthorized }) => {
             )}
           </div>
           <div className="flex items-center justify-between">
-            <div className="font-medium text-base">Competitor Activities</div>
+            <div className="font-medium text-base mr-2">Competitor Activities</div>
             {notifications.competitorActivities ? (
               <BiToggleRight
                 className="text-gray-900 cursor-pointer text-3xl ml-2"
@@ -164,7 +164,7 @@ const Settings = ({ setAuthorized }) => {
             )}
           </div>
           <div className="flex items-center justify-between">
-            <div className="font-medium text-base">Sharing Current Location</div>
+            <div className="font-medium text-base mr-2">Sharing Current Location</div>
             {notifications.currentLocation ? (
               <BiToggleRight
                 className="text-gray-900 cursor-pointer text-3xl ml-2 "
