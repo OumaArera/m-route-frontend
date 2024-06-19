5e88ca1dc144c862defb711b849e22c90291891c
OumaArera <johnouma999@gmail.com>
2024-06-19 14:33:39 +0300

    feat: render instructions

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -55,7 +55,7 @@ const SideBar = () => {
   const sidebarItems = [
     { icon: <RiHome2Line className="h-6 w-6 text-gray-900" />, label: "Home", to: "/map" },
     { icon: <RiMapPinLine className="h-6 w-6 text-gray-900" />, label: "Routes", to: "/routes" },
-    { icon: <FaRegCalendarAlt className="h-6 w-6 text-gray-900" />, label: "Calendar", to: "/calendar" },
+    { icon: <FaRegCalendarAlt className="h-6 w-6 text-gray-900" />, label: "Calendar", to: "/Create Routes" },
     { icon: <MdOutlineAddLocation className="h-6 w-6 text-gray-900" />, label: "Create Outlet", to: "/outlet" },
     { icon: <RiSettings2Line className="h-6 w-6 text-gray-900" />, label: "Settings", to: "/settings" },
     
